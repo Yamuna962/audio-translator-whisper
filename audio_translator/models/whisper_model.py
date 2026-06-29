40dc0941108ee1563cb083bb1462b6137bb7d8f3
Yamunadevi <yamunakallakuri@gmail.com>
2026-06-29 14:43:48 +0530

    Use configurable WHISPER_MODEL for loading model

--- audio_translator/models/whisper_model.py
+++ audio_translator/models/whisper_model.py
@@ -1,6 +1,12 @@
 import whisper
 import streamlit as st
 
+from audio_translator.config import WHISPER_MODEL
+
+
 @st.cache_resource
 def load_whisper_model():
-    return whisper.load_model("large")
+
+    return whisper.load_model(
+        WHISPER_MODEL
+    )
