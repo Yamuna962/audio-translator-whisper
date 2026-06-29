9cbfd8fdd0f1c49caf6f16eee0fdfadc9cf92c41
Yamunadevi <yamunakallakuri@gmail.com>
2026-06-29 14:44:29 +0530

    Refactor session state initialization and reset functions

--- audio_translator/utils/session_utils.py
+++ audio_translator/utils/session_utils.py
@@ -1,15 +1,46 @@
 import streamlit as st
 
+from audio_translator.utils.audio_utils import (
+    cleanup_previous_audio
+)
+
+
 def initialize_session_state():
 
     defaults = {
+
         "current_audio_path": None,
-        "transcription_results": {},
-        "last_file_id": None,
-        "processing_complete": False,
+
         "file_content_hash": None,
+
+        "processing_complete": False,
+
+        "transcription_results": None
+
     }
 
     for key, value in defaults.items():
+
         if key not in st.session_state:
+
             st.session_state[key] = value
+
+
+def reset_transcription_state():
+
+    st.session_state.processing_complete = False
+
+    st.session_state.transcription_results = None
+
+
+def reset_all():
+
+    cleanup_previous_audio()
+
+    st.session_state.current_audio_path = None
+
+    st.session_state.file_content_hash = None
+
+    st.session_state.processing_complete = False
+
+    st.session_state.transcription_results = None
